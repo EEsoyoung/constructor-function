8eeb2584c984253da6a671c818137fadbb13d000
leesoyoung <young357516@naver.com>
2022-08-31 18:07:44 +0900

    랜덤배열 중복없애기

--- algorithm-13.js
+++ algorithm-13.js
@@ -1,19 +1,22 @@
 //1~20까지난수 
-const inputData = [];
-//랜덤숫자 20개 받아서 inputData 배열에 넣어줌
+const randomData = [];
+//랜덤숫자 20개 받아서 randomData 배열에 넣어줌
 for(let i = 0; i < 20; i++){
   const random = Math.floor(Math.random() * 20)+1;
-  inputData.push(random);
+  randomData.push(random);
 }
-console.log(inputData)
-//랜덤 index 숫자 받아오는 배열 
+console.log(randomData)
+//랜덤 배열에서 중복을 없앤 값을 배열에넣어줌
 const answer = [];
-for(let i = 0; i < 20; i++){
-  const randomIndex = Math.floor(Math.random());
-  answer.push(inputData[randomIndex]);
-  inputData.splice(randomIndex,1);
-}
+randomData.forEach((item)=>{
+  if(!answer.includes(item)){ //answer 에 item 값이있는지 확인하고 없으면 
+    answer.push(item); // 값을 넣어준다.
+  }
+});
 console.log(answer)
+//중복을 없앤 배열에서 값을 3개 복사해옴
+const inputData = answer.slice(0,3);
+console.log(inputData);
 
 
 
