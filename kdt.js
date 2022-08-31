9d6c4e740aa0d531834922a014d6f98027067ee6
leesoyoung <young357516@naver.com>
2022-08-31 11:18:26 +0900

    생성자함수

---
+++ kdt.js
@@ -0,0 +1,25 @@
+/*export default*/ function soyoung (name,age,adress,tel){
+  if(typeof(name) === "string"
+  && typeof(age)=== "number"
+  && typeof(adress) ==="string"
+  && typeof(tel) === "number"){
+    this.name = name;
+      this.age = age;
+      this.adress = adress;
+      this.tel = tel;
+    }
+}
+const soyoungLee = new soyoung('이소영',20,"대전", 01012345678 );
+console.log(soyoungLee);
+
+// function kdt (name ,glass,paymant){
+//   this.id = name;
+//   this.b = glass;
+//   this.c = paymant;
+//   this.func = function(){
+
+//   }
+//}
+// const soyoung = new kdt ('팥','슈크림','피자');
+// console.log(typeof(soyoung));
+// console.log(soyoung);
